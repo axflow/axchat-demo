19a75bc737436f53ad7821afc21d0900c4e5c57e
Nicholas Charriere <nicholascharriere@gmail.com>
2023-09-15 16:27:35 -0700

    Use llama against our own HF endpoint

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,21 +1,33 @@
-import { OpenAIChat } from "@axflow/models/openai/chat";
+import { HuggingFaceTextGeneration } from "@axflow/models/huggingface/text-generation";
 import { StreamingJsonResponse, type MessageType } from "@axflow/models/shared";
 
 export const runtime = "edge";
 
+const buildPrompt = (messages: MessageType[]): string => {
+  let msg =
+    "Below is a message history between a user and an AI (you). Please read it and respond to the latest user question";
+  messages.forEach((message) => {
+    msg += `\n${message.role}: ${message.content}`;
+  });
+  msg += "\nassistant: ";
+  return msg;
+};
+
 export async function POST(request: Request) {
   const { messages } = await request.json();
 
-  const stream = await OpenAIChat.streamTokens(
+  const stream = await HuggingFaceTextGeneration.streamTokens(
     {
-      model: "gpt-4",
-      messages: messages.map((msg: MessageType) => ({
-        role: msg.role,
-        content: msg.content,
-      })),
+      model: "llama-2b",
+      inputs: buildPrompt(messages),
+      parameters: {
+        max_new_tokens: 200,
+      },
     },
     {
-      apiKey: process.env.OPENAI_API_KEY!,
+      accessToken: process.env.HF_TOKEN!,
+      apiUrl:
+        "https://styuqm054heenl9w.us-east-1.aws.endpoints.huggingface.cloud",
     }
   );
 
